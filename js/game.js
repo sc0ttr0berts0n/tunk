dd37909f4d5450e6f25bef1dd059c5dda0325b38
beansparrow <stvhunt132@gmail.com>
2020-02-01 03:41:52 -0500

    Added function to move player to arbitrary positions

--- js/game.js
+++ js/game.js
@@ -1,31 +1,32 @@
 class Game {
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.app = new PIXI.Application({
-            view: canvas,
-            width: window.innerWidth,
-            height: window.innerHeight,
-            transparent: true
-        });
-        this.turret = new Turret(this, 26);
-        this.player = new Player(this);
-        this.frameCount = 0;
-        this.init();
-    }
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.app = new PIXI.Application({
+      view: canvas,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      transparent: true
+    });
+    this.turret = new Turret(this, 26);
+    this.player = new Player(this);
+    this.frameCount = 0;
+    this.init();
+  }
 
-    init() {}
-    update() {
-        // console.log('update ran');
-        this.turret.update();
-    }
+  init() {}
+  update() {
+    // console.log('update ran');
+    this.turret.update();
+    this.player.update();
+  }
 }
 
-const canvas = document.getElementById('game');
+const canvas = document.getElementById("game");
 const game = new Game(canvas);
 
 const loop = () => {
-    requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 
-    game.update();
+  game.update();
 };
 loop();
