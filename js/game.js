b44482225e5a85fb8db1c0ed8e14529f1b4ca44d
beansparrow <stvhunt132@gmail.com>
2020-02-01 00:45:30 -0500

    Added Player texture

--- js/game.js
+++ js/game.js
@@ -1,25 +1,25 @@
 class Game {
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.app = new PIXI.Application({
-            view: canvas,
-            width: window.innerWidth,
-            height: window.innerHeight,
-            transparent: true
-        });
-        // this.player = new Player();
-        this.frameCount = 0;
-        this.turret = new Turret(this, 26);
-        this.init();
-    }
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.app = new PIXI.Application({
+      view: canvas,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      transparent: true
+    });
+    this.turret = new Turret(this, 26);
+    this.player = new Player(this);
+    this.frameCount = 0;
+    this.init();
+  }
 
-    init() {
-        this.app.ticker.add(animate);
-    }
-    update() {
-        this.turret.update();
-    }
+  init() {
+    //this.app.ticker.add(animate);
+  }
+  update() {
+    this.turret.update();
+  }
 }
 
-const canvas = document.getElementById('game');
+const canvas = document.getElementById("game");
 const game = new Game(canvas);
--- js/player.js
+++ js/player.js
@@ -1,11 +1,18 @@
 class Player {
-    constructor() {
-        this.el = new PIXI.Texture();
-        this.pos = { x: 0, y: 0 };
-        this.targetPos = this.pos;
-        this.alive = true;
-        this.init();
-    }
-    init() {}
-    update() {}
+  constructor(game) {
+    this.game = game;
+    this.el = new PIXI.Sprite(PIXI.Texture.from("assets/da-boi.png"));
+    this.pos = { x: 0, y: 0 };
+    this.targetPos = this.pos;
+    this.alive = true;
+    this.init();
+  }
+  init() {
+    console.log(this.game);
+    this.game.turret.el.addChild(this.el);
+    this.el.scale.set(0.3, 0.3);
+  }
+  update() {}
 }
+
+console.log("player exists and is asdfkajsdf");
