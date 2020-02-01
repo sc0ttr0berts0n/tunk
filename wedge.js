56d30004a15cd89c31480395b75b5462d5668400
cyko28 <scott.t.robertson@gmail.com>
2020-01-31 23:05:07 -0500

    added basic wedge class

---
+++ wedge.js
@@ -0,0 +1,9 @@
+class Wedge {
+    constructor(game, turret, letter, angle) {
+        this.game = game;
+        this.turret = turret;
+        this.letter = letter;
+        this.rot = angle;
+        this.shield = true;
+    }
+}
