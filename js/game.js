713fff1a9087f62c8297cd00abffa0c7f9334dbb
cyko28 <scott.t.robertson@gmail.com>
2020-02-01 00:08:07 -0500

    got classes working

--- js/game.js
+++ js/game.js
@@ -1,35 +1,25 @@
 class Game {
-    constructor() {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.app = new PIXI.Application({
+            view: canvas,
+            width: window.innerWidth,
+            height: window.innerHeight,
+            transparent: true
+        });
+        // this.player = new Player();
         this.frameCount = 0;
-        this.turret = new Turret();
-        this.player = new Player();
+        this.turret = new Turret(this, 26);
+        this.init();
+    }
+
+    init() {
+        this.app.ticker.add(animate);
+    }
+    update() {
+        this.turret.update();
     }
 }
 
 const canvas = document.getElementById('game');
-let _w = window.innerWidth;
-let _h = window.innerHeight;
-
-const app = new PIXI.Application({
-    view: canvas,
-    width: _w,
-    height: _h,
-    transparent: true
-});
-
-window.addEventListener('resize', resize);
-
-let circle = new PIXI.Graphics();
-circle.beginFill(0x9966ff);
-circle.drawCircle(0, 0, 100);
-circle.endFill();
-circle.x = _w / 2;
-circle.y = _h / 2;
-app.stage.addChild(circle);
-
-function resize() {
-    _w = window.innerWidth;
-    _h = window.innerHeight;
-
-    app.renderer.resize(_w, _h);
-}
+const game = new Game(canvas);
--- js/player.js
+++ js/player.js
@@ -1,3 +1,11 @@
 class Player {
-    constructor() {}
+    constructor() {
+        this.el = new PIXI.Texture();
+        this.pos = { x: 0, y: 0 };
+        this.targetPos = this.pos;
+        this.alive = true;
+        this.init();
+    }
+    init() {}
+    update() {}
 }
