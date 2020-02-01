871b81fac28fa4615185fafafa874bcb6f9e1eba
cyko28 <scott.t.robertson@gmail.com>
2020-02-01 18:32:32 -0500

    added random turret turning

--- js/game.js
+++ js/game.js
@@ -7,20 +7,34 @@ class Game {
             height: window.innerHeight,
             transparent: true
         });
+        this.background = new PIXI.Sprite(
+            PIXI.Texture.from('assets/da-map.png')
+        );
+        this.backgroundRot = Math.PI * 2;
+        this.backgroundTargetRot = 0;
+        this.backgroundNextMove = 0;
         // this.app.stage.filters = [new PIXI.filters.PixelateFilter()];
         // this.app.stage.filters[0].size = [1, 8];
+        this.init();
+
         this.turret = new Turret(this, 26);
         this.player = new Player(this);
         this.kb = new KeyboardObserver();
         this.damageChance = 0.0125;
         this.frameCount = 0;
-        this.init();
     }
 
-    init() {}
+    init() {
+        this.app.stage.addChild(this.background);
+        this.background.x = window.innerWidth / 2;
+        this.background.y = window.innerHeight / 2;
+        this.background.anchor.set(0.5, 0.5);
+    }
     update() {
         // console.log('update ran');
         this.frameCount++;
+
+        this.updateTurret();
 
         this.attemptDamage();
 
@@ -29,6 +43,22 @@ class Game {
             this.turret.wedges.forEach(wedge => wedge.update());
         }
         this.player.update();
+    }
+    updateTurret() {
+        const target = this.backgroundTargetRot;
+        const actual = this.backgroundRot;
+        const diff = target - actual;
+        const factor = 0.005;
+        const offset = diff * factor;
+
+        this.backgroundRot += offset;
+        this.background.rotation = this.backgroundRot;
+
+        if (this.frameCount > this.backgroundNextMove) {
+            this.backgroundTargetRot = Math.random() * (Math.PI * 4 - 2);
+            this.backgroundNextMove =
+                this.frameCount + Math.random() * 120 + 60;
+        }
     }
     attemptDamage() {
         if (Math.random() < this.damageChance) {
