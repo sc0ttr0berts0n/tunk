5f1632e9b18dc9cfc50951ac2f19951ff58c4e67
beansparrow <stvhunt132@gmail.com>
2020-02-03 13:11:50 -0500

    Changed scoring mechanism to reference wall texture change.

--- js/keyboard-observer.js
+++ js/keyboard-observer.js
@@ -1,15 +1,20 @@
 class KeyboardObserver {
     constructor() {
         this.key = null;
-        this.code = null;
+        this.code = 65;
         this.init();
     }
     init() {
         document.addEventListener('keydown', this.handleKeyPress.bind(this));
     }
     handleKeyPress(e) {
-        console.log("KeyPress = " + e.which);
-        if ((e.which >= 65 && e.which <= 90) || (e.which == 37) || (e.which == 38) || (e.which == 39)) {
+        console.log('KeyPress = ' + e.which);
+        if (
+            (e.which >= 65 && e.which <= 90) ||
+            e.which == 37 ||
+            e.which == 38 ||
+            e.which == 39
+        ) {
             this.key = e.key;
             this.code = e.which;
         }
