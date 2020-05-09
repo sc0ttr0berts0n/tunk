26f209ca42cb937fd65b70f1856ec38e2685ad82
Scott Robertson <scott.t.robertson@gmail.com>
2020-05-09 03:35:17 -0400

    goon can be killed by flak

--- js/goon.js
+++ js/goon.js
@@ -8,8 +8,7 @@ class Goon {
         const goon = this.game.graphics.goon;
         goon.anchor.set(0.5, 0.5);
         goon.scale.set(0.75, 0.75);
-        goon.x = Math.random() * this.game.app.renderer.width;
-        goon.y = Math.random() * this.game.app.renderer.height;
+        this.spawn();
     }
     update() {
         const player = this.game.graphics.player.worldTransform;
@@ -17,12 +16,39 @@ class Goon {
         const xDist = player.tx - goon.tx;
         const yDist = player.ty - goon.ty;
         const hypot = Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-        const factor = 0.001;
+        const factor = 0.0025;
+
+        // move goon
         this.game.graphics.goon.x += xDist * factor;
         this.game.graphics.goon.y += yDist * factor;
 
+        // player death check
         if (hypot < 50 && hypot !== 0) {
             this.game.player.alive = false;
         }
+
+        if (this.game.flaks.length > 0) {
+            this.game.flaks.forEach((flak) => {
+                const goon = this.game.graphics.goon.worldTransform;
+                const pos = flak.el.worldTransform;
+                const xDist = goon.tx - pos.tx;
+                const yDist = goon.ty - pos.ty;
+                const dist = Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+                console.log(dist);
+
+                if (dist < 40) {
+                    this.spawn();
+                }
+            });
+        }
+    }
+    spawn() {
+        const sign = () => Math.sign(Math.random() - 0.5);
+        this.game.graphics.goon.x =
+            sign() * this.game.app.renderer.width +
+            (Math.random() * this.game.app.renderer.width) / 4;
+        this.game.graphics.goon.y =
+            sign() * this.game.app.renderer.height +
+            (Math.random() * this.game.app.renderer.height) / 4;
     }
 }
